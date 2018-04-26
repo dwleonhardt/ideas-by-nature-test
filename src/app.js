be459e6dec522234295432b73f44080325e661e6
dwleonhardt <dwleonhardt@gmail.com>
2018-04-26 12:13:10 -0600

    add react router dom functionality

--- src/app.js
+++ src/app.js
@@ -4,10 +4,16 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Exchange from './components/Exchange';
 import MarketTrends from './components/MarketTrends';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={ store }>
-      <Exchange />
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Exchange}/>
+          <Route exact path="/trends" component={MarketTrends}/>
+        </div>
+      </BrowserRouter>
     </Provider>,
 
     document.getElementById('root'));
