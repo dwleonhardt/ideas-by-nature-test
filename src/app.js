da4527226ebed734eb55f8d2e9b848e9778e595f
dwleonhardt <dwleonhardt@gmail.com>
2018-04-26 11:51:04 -0600

    remove router dependancy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
 import Exchange from './components/Exchange';
 import MarketTrends from './components/MarketTrends';
