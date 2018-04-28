dc82a7eaa0ccb00afae42cf664f63a2853c7c052
dwleonhardt <dwleonhardt@gmail.com>
2018-04-28 15:00:20 -0600

    add display time range

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -5,34 +5,16 @@ import moment from 'moment';
 import * as d3 from 'd3';
 import style from '../style/graph.css';
 
-
-// Since this is "time series" visualization, our x axis should have a time scale.
- // Our x domain will be the extent ([min, max]) of x values (Dates) in our data set.
- // Our x range will be from x=0 to x=width.
-
-
- // Our y axis should just have a linear scale.
- // Our y domain will be the extent of y values (numbers) in our data set.
-
- //
- // // These two functions select the scaled x and y values (respectively) of our data.
-
- //
-
-
 class Graph extends Component {
   getPriceByDate () {
-    let date = new Date();
-    let today = date.getDay();
-    let sunday = date.getDate() - today + (today === 0 ? -6:-1);
-    let weekStart = moment.utc(date.setDate(sunday)).format();
-    let saturday = new Date().getDate() - today + (today === 0 ? -6:11);
-    let weekEnd = moment.utc(date.setDate(saturday)).format();
-    fetch(`https://ideas-by-nature-test.herokuapp.com/price_date?start=${weekStart}&end=${weekEnd}&currency=BTC`)
+    let dayStart = moment.utc(moment().startOf('day')).format();
+    let dayEnd = moment.utc(moment().endOf('day')).format();
+    fetch(`https://ideas-by-nature-test.herokuapp.com/price_date?start=${dayStart}&end=${dayEnd}&currency=BTC`)
     .then((response) => response.json())
     .then((prices) => {
       prices.map((p) => {
         p.price = new Number(p.price).toFixed(2);
+        p.time = new Date(p.time);
       })
       this.props.PriceGraph(prices);
       this.createGraph();
@@ -54,16 +36,16 @@ class Graph extends Component {
     const barWidth = new Number(width / this.props.data.length);
     for (var i = 0; i < this.props.data.length; i++) {
       price.push(new Number(this.props.data[i].price));
-      time.push(new Date(this.props.data[i].time));
+      time.push(this.props.data[i].time);
     }
-    console.log(d3);
     const y = d3.scaleLinear()
     .domain([d3.min(price) -5, d3.max(price) +5])
     .range([height, 0]);
 
-    const x = d3.scaleLinear()
+    const x = d3.scaleTime()
     .domain([d3.min(time), d3.max(time)])
     .range([0, width]);
+
 
     const chart = d3.select(".container___3wpGl")
     .attr("width", width + margin.left + margin.right)
@@ -82,7 +64,7 @@ class Graph extends Component {
     const yTicks = y.ticks(),
     yTickFormat = y.tickFormat(function(d) { return "$" + d; }),
     xTicks = x.ticks(),
-    xTickFormat = x.tickFormat(function(d) { return d; })
+    xTickFormat = x.tickFormat(d3.timeHour(time));
 
     yTicks.map(yTickFormat);
     xTicks.map(xTickFormat);
@@ -93,14 +75,8 @@ class Graph extends Component {
       .call(xAxis);
 
     chart.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
+      .attr("class", "y axis")
+      .call(yAxis);
 
     bar.append("rect")
     .attr("y", function(d) {
